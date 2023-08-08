e37f0dc5b0c1f78b7002926ff3c075a721b0fc5c
RodinDima <rodindima1984@gmail.com>
2023-08-08 22:44:38 +0300

    1111

---
+++ src/components/Section1/Section1.jsx
@@ -0,0 +1,17 @@
+import './style.css';
+import { ReactComponent as Auto } from './image/auto.svg';
+
+
+const Section1 = () => {
+  return (
+    <>
+       <div>test</div>
+       <Auto/>
+       <div></div>
+       <div className="box"></div>
+
+    </>
+  );
+}
+
+export default Section1;
